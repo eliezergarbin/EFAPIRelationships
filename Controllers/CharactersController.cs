a6b653d6a6f9d8f7b0673e05be51e3041b7c1f7b
Eliezer Nascimento <eliezergarbin1@gmail.com>
2023-01-16 14:09:24 -0300

    adicionando metodos de Get e Create

---
+++ Controllers/CharactersController.cs
@@ -0,0 +1,36 @@
+﻿using EFAPIRelationships.Data;
+using EFAPIRelationships.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFAPIRelationships.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharactersController : Controller
+    {
+        private readonly DataContext _context;
+
+        public CharactersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Character>>> Get(int userId)
+        {
+            var characters = await _context.Characters.Where(x => x.UserId == userId).ToListAsync();
+
+            return characters;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Character>>> Create(Character character)
+        {
+            _context.Characters.Add(character);
+            await _context.SaveChangesAsync();
+
+            return await Get(character.UserId);
+        }
+    }
+}
